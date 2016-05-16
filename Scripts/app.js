ac950db75faf7981ded41e1b6fe3590d6c5f3c0f
larizocche <lari.zocche@gmail.com>
2016-05-16 09:54:03 -0400

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,11 @@
+/* main JavaScript file */
+
+//IIFE  - Immediately Invoked Function Expression
+(function(){  
+    "use strict";
+    //code goes in here
+    console.log("App Started...");
+    
+    
+    
+})();
